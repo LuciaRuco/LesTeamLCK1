a440d6b57795855ecf490d87bd2535e0c8ec44b0
Clinton-Uachave <cuachave@unilurio.ac.mz>
2020-03-26 01:24:28 -1200

    2parte

--- src/main/java/com/example/demo/PrincipalMain.java
+++ src/main/java/com/example/demo/PrincipalMain.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrincipalMain {
+public class PrincipalMain {.
+
 
     public static void main(String[] args) {
 

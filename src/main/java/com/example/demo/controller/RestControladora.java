635cfdcd32df6af1eca697ed00cc44674289d079
lruco <lruco@unilurio.ac.mz>
2020-04-12 22:58:56 +0200

    Power of two

--- src/main/java/com/example/demo/controller/RestControladora.java
+++ src/main/java/com/example/demo/controller/RestControladora.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
+import org.json.JSONArray;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,4 +24,33 @@ public class RestControladora {
         map.put(new String[]{"number :", "2","decomposition :" ,"[ 2, 2, 2, 2 ]"});
     return new ResponseEntity<Object>(map,HttpStatus.OK);
     }*/
+
+
+
+    /*
+    este metodo tem como url de mapeameto o "server/primeFactors" e tem paramentro number que
+     recebe um inteiro e eh acedido da seguinte maneira ("server/primeFactors?number=20") e
+     devolve um JSON contendo o number e um array da decomposicao do number
+    * */
+    @GetMapping("/primeFactors")
+    public ResponseEntity<Object> primeFactors(@RequestParam(value = "number", defaultValue = "1") int number) {
+
+        JSONArray ret= new JSONArray();
+        int result=number;
+
+
+        while (result!=1){
+            int divid=2;
+            while (result%divid != 0){
+                divid++;
+            }
+            ret.put(divid);
+            result = result/divid;
+        }
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("number",number);
+        map.put("decomposition", ret.toString());
+        return new ResponseEntity<Object>(map,HttpStatus.OK);
+    }
+
 }
